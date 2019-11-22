2b3c895329ba374bcfedcc35937a1e07ce97d8f6
bdestasio33 <bdestasio33@gmail.com>
2019-11-22 09:48:47 -0500

    Completed module

--- StarChart/Data/ApplicationDbContext.cs
+++ StarChart/Data/ApplicationDbContext.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using StarChart.Models;
 
 namespace StarChart.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<CelestialObject> CelestialObject { get; set; }
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
+            
         }
     }
 }
